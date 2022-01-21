a860ed1a04a2aa215e04e291886ef7606a7976a5
Soner Çökmen <soner.cokmen@trendyol.com>
2022-01-21 11:33:43 +0300

    style(core): add jsdoc to object and date generators

--- packages/core/src/generators/date.ts
+++ packages/core/src/generators/date.ts
@@ -1,10 +1,32 @@
 import { DateStream } from '../streams/stream-loader';
 import { DateUtils } from '../utils/date-utils';
 
+/**
+ * Creates a `DateStream` that generates a date with the given boundaries.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/date-generators#date-min-max|Docs}
+ * @param {Date} [min=now] - the minimum of the boundary
+ * @param {Date} [max=tomorrow] - the maximum of the boundary
+ * @returns {DateStream} DateStream
+ */
 export const date = (min: Date = DateUtils.getToday(), max: Date = DateUtils.getTomorrow()) => DateStream.between(min, max);
 
+/**
+ * Creates a `DateStream` that generates a date that is the date of now.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/date-generators#now|Docs}
+ * @returns {DateStream} DateStream
+ */
 export const now = (): DateStream => DateStream.fromDate(DateUtils.now());
 
+/**
+ * Creates a `DateStream` that generates a date that is the date of tomorrow.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/date-generators#tomorrow|Docs}
+ * @returns {DateStream} DateStream
+ */
 export const tomorrow = (): DateStream => DateStream.fromDate(DateUtils.getTomorrow());
 
+/**
+ * Creates a `DateStream` that generates a date that is the date of yesterday.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/date-generators#yesterday|Docs}
+ * @returns {DateStream} DateStream
+ */
 export const yesterday = (): DateStream => DateStream.fromDate(DateUtils.getYesterday());
--- packages/core/src/generators/object.ts
+++ packages/core/src/generators/object.ts
@@ -1,4 +1,10 @@
 import { ObjectModel } from '../types/object-model';
 import { ObjectStream } from '../streams/stream-loader';
 
+/**
+ * Creates an `ObjectStream` that generates an object with the given mode.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/object-generators#obj-model|Docs}
+ * @param {*} [model] - a key-value object model that all keys are an instance of a factory
+ * @returns {ObjectStream} ObjectStream
+ */
 export const obj = <T = any>(model: ObjectModel<T>): ObjectStream<T> => ObjectStream.of(model);
