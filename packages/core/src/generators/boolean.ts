f1230be29c2d81ce1fddf232ddc77be26c05835e
Soner Çökmen <soner.cokmen@trendyol.com>
2022-01-21 10:33:29 +0300

    style(core): add jsdoc to boolean and number generators

--- packages/core/src/generators/boolean.ts
+++ packages/core/src/generators/boolean.ts
@@ -1,8 +1,24 @@
 import { DEFAULT_PERCENTAGE } from '../constants/limits';
 import { BooleanStream } from '../streams/stream-loader';
 
+/**
+ * Creates a `BooleanStream` that generates always false.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/boolean-generators#falsy|Docs}
+ * @returns {BooleanStream} BooleanStream
+ */
 export const falsy = (): BooleanStream => BooleanStream.falsy();
 
+/**
+ * Creates a `BooleanStream` that generates always true.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/boolean-generators#truthy|Docs}
+ * @returns {BooleanStream} BooleanStream
+ */
 export const truthy = (): BooleanStream => BooleanStream.truthy();
 
+/**
+ * Creates a `BooleanStream` that generates a boolean with the given percentage.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/boolean-generators#bool|Docs}
+ * @param {number} [percentage=0.5] - a number within [0, 1] of how often the result should be true.
+ * @returns {BooleanStream} BooleanStream
+ */
 export const bool = (percentage: number = DEFAULT_PERCENTAGE): BooleanStream => BooleanStream.fromPercentage(percentage);
--- packages/core/src/generators/number.ts
+++ packages/core/src/generators/number.ts
@@ -2,12 +2,42 @@ import { DEFAULT_MAX_NUMBER, DEFAULT_MIN_NUMBER } from '../constants/limits';
 import { NumberStream } from '../streams/stream-loader';
 import { IntegerFactory } from '../factories/integer-factory';
 
+/**
+ * Creates a `NumberStream` that generates a float number with the given boundaries.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/number-generators#real-min-max|Docs}
+ * @param {number} [min=0] - the minimum of the boundary
+ * @param {number} [max=1000] - the maximum of the boundary
+ * @returns {NumberStream} NumberStream
+ */
 export const real = (min: number = DEFAULT_MIN_NUMBER, max: number = DEFAULT_MAX_NUMBER): NumberStream => NumberStream.between(min, max);
 
+/**
+ * Creates a `NumberStream` that generates an integer number with the given boundaries.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/number-generators#int-min-max|Docs}
+ * @param {number} [min=0] - the minimum of the boundary
+ * @param {number} [max=1000] - the maximum of the boundary
+ * @returns {NumberStream} NumberStream
+ */
 export const int = (min: number = DEFAULT_MIN_NUMBER, max: number = DEFAULT_MAX_NUMBER): NumberStream => new NumberStream(new IntegerFactory(min, max));
 
-export const num = (num: number): NumberStream => NumberStream.constant(num);
+/**
+ * Creates a `NumberStream` that generates always the given number.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/number-generators#num-val|Docs}
+ * @param {number} [val] - the number to be generated.
+ * @returns {NumberStream} NumberStream
+ */
+export const num = (val: number): NumberStream => NumberStream.constant(val);
 
+/**
+ * Creates a `NumberStream` that generates always zero.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/number-generators#zero|Docs}
+ * @returns {NumberStream} NumberStream
+ */
 export const zero = (): NumberStream => NumberStream.constant(0);
 
+/**
+ * Creates a `NumberStream` that generates always one.
+ * @see {@link https://scokmen.gitbook.io/fluent-fixture/concepts/generators/number-generators#one|Docs}
+ * @returns {NumberStream} NumberStream
+ */
 export const one = (): NumberStream => NumberStream.constant(1);
