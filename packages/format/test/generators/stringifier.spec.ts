f9a247d0a89f279140073b3f3b1e46510d9a2aac
Soner Çökmen <soner.cokmen@trendyol.com>
2022-03-18 09:41:33 +0300

    chore(format): remove time-zone dependent test case

--- packages/format/test/generators/stringifier.spec.ts
+++ packages/format/test/generators/stringifier.spec.ts
@@ -10,7 +10,6 @@ describe('Stringifier', () => {
       [null, ''],
       [true, 'true'],
       [1, '1'],
-      [new Date(1_647_526_028_815), 'Thu Mar 17 2022 17:07:08 GMT+0300 (GMT+03:00)'],
     ];
     test.each(inputs)('should covert the given input into a string: %p', (input: any, output: string) => {
       const generator = new Stringifier();
