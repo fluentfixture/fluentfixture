0dffff0acb752aaec4ee232769c4b983eb0c1c70
Soner Çökmen <soner.coekmen@zalando.de>
2024-10-13 15:55:28 +0200

    docs: update readme files and change e-commerce sample

--- sample/core/src/e-commerce/shopping-cart.ts
+++ sample/core/src/e-commerce/shopping-cart.ts
@@ -1,8 +1,8 @@
-import { alphabetic, alphanumeric, bool, hex, int, obj, pick, val } from '@fluentfixture/core';
+import { alphabetic, bool, hex, int, obj, pick } from '@fluentfixture/core';
 
 // Defines a price generator with amount and the currency fields.
 const price = obj({
-  amount: int(100, 1000),
+  amount: int(100, 1000).add(0.95),
   currency: pick(['USD', 'EUR', 'GBP', 'TRY']),
 });
 
@@ -11,27 +11,32 @@ const color = hex(6).padStart(7, '#').upperCase();
 
 // Defines a category with constant id.
 const category = obj({
-  id: val(5),
-  code: alphanumeric().upperCase()
+  id: int(1, 100),
+  name: alphabetic().headerCase(),
+  type: alphabetic(4, 8).memo()
 })
 
 // Defines a product generator.
 const product = obj({
   id: int(1, 999),
   name: alphabetic(10, 20).capitalCase(),
-  category: category.memo(),
+  category: category,
   description: alphabetic(20, 40).optional(),
+  hasStock: bool(0.5),
   color: color,
   price: price,
-  hasStock: bool(0.5),
 });
 
-// Adds 'code' field using generated values.
-// Iterates the model.
-// Sorts the generated models.
+// 1) Adds 'code' field using generated values.
+// 2) Iterates the model.
+// 3) Sorts the generated models by their prices.
 const products = product
   .lazy('code', (p) => `${p.id}-${p.color}`)
   .array(10)
   .sort((a, b) => a.price.amount - b.price.amount);
 
+// Print all products
 console.log(products.single());
+
+// Print details of the first product.
+console.log(product.format('[${id}] ${name:titleCase()} => ${price.amount}'));
